c55efb3661cb0fa0ba2e96c522fe48085cfff6e5
Kikita <local.mail>
2022-07-29 13:30:41 +0000

    add touching ground

--- Assets/Scripts/Game/Hero.cs
+++ Assets/Scripts/Game/Hero.cs
@@ -8,14 +8,19 @@ public class Hero : MonoBehaviour
     [SerializeField] private float speed = 0.5f;
     [SerializeField] private float jumpForce = 0.1f;
     Rigidbody2D rb;
-    Collider2D[] res;
+    Collider2D[] res = new Collider2D[3];
+    ContactFilter2D filter;
     private float lastRun = 0, lastJump = 0;
     [SerializeField] private float runCD = 0.1f, jumpCD = 0.5f;
-    private bool mirrored = false;
+    private int direction = 0;
+    private bool canRun = false, canJump = false, canDoubleJump = true;
+    private int count = 0;
+    private bool touchesSmth = false;
     
 
     void Start()
     {
+        filter.NoFilter();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -38,41 +43,76 @@ public class Hero : MonoBehaviour
     private void Jump()
     {
         if ((Time.timeSinceLevelLoad - lastJump) > jumpCD){
+            if (!canJump){
+                canDoubleJump = false;
+            }
             rb.velocity += new Vector2(0, jumpForce);
             lastJump = Time.timeSinceLevelLoad;
         }
     }
 
-    private void Mirror(){
-        mirrored = !mirrored;
+    private void chooseDirection(int newDirection){
+        if (newDirection == direction)
+            return;
+
+        direction = newDirection;
         transform.RotateAround(this.transform.position, Vector3.up, 180);
     }
 
     private void CheckGround() 
     {
+        for (int i = 0; i < count; i++)
+        {
+            res[i] = new Collider2D();
+        }
 
+        count = rb.OverlapCollider( filter, res );
+        touchesSmth = !(count == 0);
+
+        if (!touchesSmth){
+            canRun = false;
+            canJump = false;
+            return;
+        }
+
+        for (int i = 0; i < count; i++){
+            if (res[i].attachedRigidbody.gameObject.name == "Ground"){
+                canRun = true;
+                canJump = true;
+                canDoubleJump = true;
+                break;
+            }
+        }
     }
     void FixedUpdate()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && (canJump || canDoubleJump ) )
         {
             Jump();
         }
 
         if (Input.GetKey("d"))
         {
-            RunToRight();
+            chooseDirection(1);
+            if (canRun)
+                RunToRight();
         }
 
         if (Input.GetKey("a"))
         {
-            RunToLeft();
+            chooseDirection(0);
+            if (canRun)
+                RunToLeft();
         }
 
 
-        if ( ( (rb.velocity.x > 0) && !mirrored ) || ( (rb.velocity.x < 0) && mirrored ) ){
-           Mirror();
-        } 
+        if (rb.velocity.x > 0){
+           chooseDirection(1);
+        } else if (rb.velocity.x < 0){
+            chooseDirection(0);
+        }
 
+        CheckGround();
+        Debug.Log(canRun);
     }
 }
