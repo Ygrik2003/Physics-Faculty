dfdf8f5cf34363d0079e77aefa1fb67f9c8b14f3
Maljajew <maljajew@gmail.com>
2022-07-30 13:41:34 +0300

    Mipo

--- Assets/Scripts/Menu/SnapScrollInfo.cs
+++ Assets/Scripts/Menu/SnapScrollInfo.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class SnapScrollInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(1, 50)]
+    [Header("Controllers")]
+    public int panCount;
+    [Range(0, 500)]
+    public int panOffset;
+    [Range(0f, 20f)]
+    public float snapSpeed;
+    [Range(0f, 5f)]
+    public float scaleOffset;
+    [Header("Other Objects")]
+    public GameObject panPrefab;
+
+    private GameObject[] instPans;
+    private Vector2[] pansPos;
+    private Vector2[] pansScale;
+
+    private RectTransform contentRect;
+    private Vector2 contentVector;
+    
+    private int selectedPanID;
+    private bool isScrolling;
+
+    private void Start()
     {
-        
+        contentRect = GetComponent<RectTransform>();
+        instPans = new GameObject[panCount];
+        pansPos = new Vector2[panCount];
+        pansScale = new Vector2[panCount];
+        for ( int i = 0; i < panCount; i++)
+        {
+            instPans[i] = Instantiate(panPrefab, transform, false);
+            if (i == 0) continue;
+            instPans[i].transform.localPosition = new Vector2(instPans[i - 1].transform.localPosition.x + 
+                panPrefab.GetComponent<RectTransform>().sizeDelta.x + panOffset, instPans[i].transform.localPosition.y);
+            pansPos[i] = -instPans[i].transform.localPosition;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float nearestPos = float.MaxValue;
+        for (int i = 0; i < panCount; i++)
+        {
+            float distance = Mathf.Abs(contentRect.anchoredPosition.x - pansPos[i].x);
+            if (distance < nearestPos)
+            {
+                nearestPos = distance;
+                selectedPanID = i;
+            }
+            float scale = Mathf.Clamp(1 / (distance / panOffset) * scaleOffset, 0.5f, 1f);
+            if (instPans[i].transform.localScale.x == scale || instPans[i].transform.localScale.y == scale)
+            {
+                pansScale[i].x = Mathf.SmoothStep(instPans[i].transform.localScale.x, scale, 6 * Time.fixedDeltaTime);
+                pansScale[i].y = Mathf.SmoothStep(instPans[i].transform.localScale.y, scale, 6 * Time.fixedDeltaTime);
+                instPans[i].transform.localScale = pansScale[i];
+            }
+        }
+        if (isScrolling) return;
+        contentVector.x = Mathf.SmoothStep(contentRect.anchoredPosition.x, pansPos[selectedPanID].x, 
+            snapSpeed * Time.fixedDeltaTime);
+        contentRect.anchoredPosition = contentVector;
+    }
+    public void Scrolling(bool scroll)
+    {
+        isScrolling = scroll;
     }
 }
