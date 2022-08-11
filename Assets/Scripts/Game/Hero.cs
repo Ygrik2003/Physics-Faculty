e8a0a878850b05cff8092232c4ed2df867db2257
Maljajew <maljajew@gmail.com>
2022-08-11 21:29:19 +0300

    Reworking the Hero abilities

--- Assets/Scripts/Game/Hero.cs
+++ Assets/Scripts/Game/Hero.cs
@@ -1,73 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static System.Convert;
 
 public class Hero : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.5f;
-    [SerializeField] private float jumpForce = 0.1f;
+    public Vector2 moveVector;
+    public int speed = 5;
+
+    public int jumpForce = 10;
+    private bool jumpControl;
+    private int jumpIteration = 0;
+    public int jumpValueIteration = 60; //fps
+
+    public int lungeImpulse = 5000;
+    private bool lockLunge = false;
+    
+    public bool faceRight = true;
+    public Rigidbody2D rb;
+    
+    //We will have animations later
+    //public Animator anim;
+    
+    public bool onGround;
+    public Transform GroundCheck;
+    private float GroundCheckRadius;
+    public LayerMask Ground;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        GroundCheckRadius = GroundCheck.GetComponent<CircleCollider2D>().radius;
+    }
+
+    private void Update()
+    {
+        Walk();
+        Reflect();
+        Jump();
+        Lunge();
+        CheckingGround();
+    }
+
+    void Walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
+
+        //anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
+    }
+
+    void Reflect()
+    {
+        if ((moveVector.x > 0 && !faceRight) || (moveVector.x < 0 && faceRight))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+            faceRight = !faceRight;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if (onGround) 
+            {
+                jumpControl = true;
+            }
+        }
+        else
+        {
+            jumpControl = false;
+        }
+        if (jumpControl)
+        {
+            if (jumpIteration++ < jumpValueIteration)
+            {
+                rb.AddForce(Vector2.up * jumpForce / jumpIteration);
+            }
+        }
+        else
+        {
+            jumpIteration = 0;
+        }
+    }
+
+    void Lunge()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !lockLunge)
+        {
+            //anim.StopPlayback();
+            //anim.Play("lunge");
+
+            lockLunge = true;
+            Invoke("LungeLock", 2f);
+
+            rb.velocity = new Vector2(0, 0);
+
+            if(!faceRight)
+            {
+                rb.AddForce(Vector2.left * lungeImpulse);
+            }
+            else
+            {
+                rb.AddForce(Vector2.right * lungeImpulse);
+            }
+        }
+    }
+
+    void LungeLock()
+    {
+        lockLunge = false;
+    }
+
+    void CheckingGround()
+    {
+        onGround = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, Ground);
+        //anim.SetBool("onGround", onGround);
+    }
+    //-------------------------------------------------------------------------------------------------------------------------------------------
+    /*[SerializeField] public float speed = 0.5f;
+    [SerializeField] public float jumpForce = 0.7f;
     Rigidbody2D rb;
     Collider2D[] res = new Collider2D[3];
     ContactFilter2D filter;
-    private float lastRun = 0, lastJump = 0;
-    [SerializeField] private float runCD = 0.1f, jumpCD = 0.5f;
     private int direction = 0;
     private bool canRun = false, canJump = false, canDoubleJump = true;
     private int count = 0;
     private bool touchesSmth = false;
 
-    public Vector2 moveVector;
-    
+    public Vector2 moveVector;*/
 
-    void Start()
+    /*private void Jump()
     {
-        filter.NoFilter();
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void walk()
-    {
-        moveVector.x = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveVector.x * speed, rb.velocity.y);
-    }
-
-    /*private void RunToRight()
-    {
-        if ((Time.timeSinceLevelLoad - lastRun) > runCD){
-            rb.velocity += new Vector2(speed, 0);
-            lastRun = Time.timeSinceLevelLoad;
-        }
-    }
-
-    private void RunToLeft()
-    {
-        if ((Time.timeSinceLevelLoad - lastRun) > runCD){
-            rb.velocity -= new Vector2(speed, 0);
-            lastRun = Time.timeSinceLevelLoad;
-        }
-    }*/
-
-    private void Jump()
-    {
-        if ((Time.timeSinceLevelLoad - lastJump) > jumpCD){
+        /*if ((Time.timeSinceLevelLoad - lastJump) > jumpCD){
             if (!canJump){
                 canDoubleJump = false;
             }
             rb.velocity += new Vector2(0, jumpForce);
             lastJump = Time.timeSinceLevelLoad;
         }
-    }
+    }*/
 
-    private void chooseDirection(int newDirection){
+    /*private void chooseDirection(int newDirection){
         if (newDirection == direction)
             return;
 
         direction = newDirection;
         transform.RotateAround(this.transform.position, Vector3.up, 180);
-    }
+    }*/
 
-    private void CheckGround() 
+    /*private void CheckGround() 
     {
         for (int i = 0; i < count; i++)
         {
@@ -98,30 +182,12 @@ public class Hero : MonoBehaviour
                 break;
             }
         }
-    }
-    void FixedUpdate()
+    }*/
+
+    /*void FixedUpdate()
     {
-        if (Input.GetKey("space") && (canJump || canDoubleJump ) )
-        {
-            Jump();
-        }
-
         walk();
-
-        /*if (Input.GetKey("d"))
-        {
-            chooseDirection(1);
-            if (canRun)
-                RunToRight();
-        }
-
-        if (Input.GetKey("a"))
-        {
-            chooseDirection(0);
-            if (canRun)
-                RunToLeft();
-        }*/
-
+        Jump();
 
         if (rb.velocity.x > 0){
            chooseDirection(1);
@@ -130,6 +196,5 @@ public class Hero : MonoBehaviour
         }
 
         CheckGround();
-        Debug.Log(canRun);
-    }
+        Debug.Log(canRun);*/
 }
